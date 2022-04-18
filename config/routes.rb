b3a4a6c0069a6f790186506c19d2e74bc1cc8960
cbulzoni <cbulzoni@chicagobooth.edu>
2022-04-18 23:49:29 +0000

    Added followers and following list pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   
   get ":username/liked" => "photos#liked", as: :liked_photos
   get ":username/feed" => "photos#feed", as: :feed
-  # get ":username/followers" => "photos#liked", as: :liked_photos
-  # get ":username/following" => "photos#liked", as: :liked_photos
+  get ":username/followers" => "follow_requests#followers", as: :followers
+  get ":username/following" => "follow_requests#leaders", as: :leaders
 
   get "/:username" => "users#show", as: :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
