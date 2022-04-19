a99a3f26ead9f2a54d76982dc5257feabc1cb970
cbulzoni <cbulzoni@chicagobooth.edu>
2022-04-19 00:37:56 +0000

    Removed photo counts from posts, liked photos and feed

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
   get ":username/following" => "follow_requests#leaders", as: :leaders
 
   get "/:username" => "users#show", as: :user
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
